e7301faa751e6f91a10f477ee1dd00feb0076cae
DannyT <dan@moov2.com>
2015-06-25 16:30:26 +0100

    added image upload and age detection

---
+++ Pubhack4/Domain/Face.cs
@@ -0,0 +1,8 @@
+﻿namespace Pubhack4.Domain
+{
+    public class Face
+    {
+        public double Age { get; set; }
+        public string Gender { get; set; }
+    }
+}
---
+++ Pubhack4/Modules/FaceModule.cs
@@ -0,0 +1,29 @@
+﻿using Nancy;
+using Pubhack4.Domain;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+
+namespace Pubhack4.Modules
+{
+    public class FaceModule : NancyModule
+    {
+        public FaceModule(IRootPathProvider pathProvider)
+        {
+            Post["/api/face/upload", true] = async (parameters, ct) =>
+            {
+                var file = this.Request.Files.FirstOrDefault();
+
+                if (file == null)
+                    return HttpStatusCode.BadRequest;
+
+                var faceService = new Pubhack4.Services.Face.FaceService();
+                var faces = await faceService.GetFacesFromImage(file.Value);
+
+                return Response.AsJson(faces);
+            };
+        }
+    }
+}
