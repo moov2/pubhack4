9d243f15927c4ea56bdace7ca76157dc0f4731cf
Peter Keating <peter@moov2.com>
2015-06-25 10:30:48 +0100

    setup Nancy with example module.

---
+++ Pubhack4/Modules/FeedModule.cs
@@ -0,0 +1,16 @@
+﻿using Nancy;
+using System;
+
+namespace Pubhack4.Modules
+{
+    public class FeedModule : NancyModule
+    {
+        public FeedModule()
+        {
+            Get["/api/feed"] = _ =>
+            {
+                return Response.AsJson<bool>(true);
+            };
+        }
+    }
+}
