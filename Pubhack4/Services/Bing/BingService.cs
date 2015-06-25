aec89900d3cdec47b2f2e367c3b7a088162d681e
JoeMoov2 <joe.berkley@moov2.com>
2015-06-25 15:42:28 +0100

    Added debug switcch for Bing API

--- Pubhack4/Services/Bing/BingService.cs
+++ Pubhack4/Services/Bing/BingService.cs
@@ -11,6 +11,8 @@ namespace Pubhack4.Services.Bing
     public class BingService
     {
 
+        private static bool debug = true;
+
         public BingService()
         {
         }
@@ -19,37 +21,43 @@ namespace Pubhack4.Services.Bing
         {
             string ImgPath = "";
 
-            // Create a Bing container.
-
-            string rootUrl = "https://api.datamarket.azure.com/Bing/Search";
-
-            var bingContainer = new BingSearchContainer(new Uri(rootUrl));
-
-            // The market to use.
-
-            string market = "en-us";
-
-            // Configure bingContainer to use your credentials.
-
-            bingContainer.Credentials = new NetworkCredential(Config.BingApiKey, Config.BingApiKey);
-
-            // Build the query, limiting to 10 results.
-
-            var imageQuery =
-
-            bingContainer.Image(SearchTerm, null, market, null, null, null, null);
-
-            imageQuery = imageQuery.AddQueryOption("$top", 1);
-
-            // Run the query and display the results.
-
-            var imageResults = imageQuery.Execute();
-
-            foreach(var item in imageResults)
+            if(!debug)
             {
-                ImgPath = item.MediaUrl;
-            }
+                // Create a Bing container.
 
+                string rootUrl = "https://api.datamarket.azure.com/Bing/Search";
+
+                var bingContainer = new BingSearchContainer(new Uri(rootUrl));
+
+                // The market to use.
+
+                string market = "en-us";
+
+                // Configure bingContainer to use your credentials.
+
+                bingContainer.Credentials = new NetworkCredential(Config.BingApiKey, Config.BingApiKey);
+
+                // Build the query, limiting to 10 results.
+
+                var imageQuery =
+
+                bingContainer.Image(SearchTerm, null, market, null, null, null, null);
+
+                imageQuery = imageQuery.AddQueryOption("$top", 1);
+
+                // Run the query and display the results.
+
+                var imageResults = imageQuery.Execute();
+
+                foreach(var item in imageResults)
+                {
+                    ImgPath = item.MediaUrl;
+                }
+            }
+            else
+            {
+                ImgPath = "http://greentreesarborcareinc.com/wp-content/uploads/2014/01/image-placeholder.jpg";
+            }
             return ImgPath;
         }
     }
